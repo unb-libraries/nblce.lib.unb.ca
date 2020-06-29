71707d30059c941410614a92bb0994e631ceb7ce
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2020-06-29 11:46:20 -0300

    NBLCE-84 Add empty search check to JS library

--- custom/themes/nblce_lib_unb_ca/src/js/search.js
+++ custom/themes/nblce_lib_unb_ca/src/js/search.js
@@ -9,7 +9,10 @@
     Drupal.behaviors.nblce_lib_unb_ca = {
         attach: function (context, settings) {
             $(".search-content .view-empty span#search-query ").text(function() {
-                return $(this).text().replace("your search", "'" + $("input[name='query']").attr('value') + "'" );
+                let $query = $("input[name='query']").attr('value');
+                if ($query !== '') {
+                    return $(this).text().replace("your search", "'" + $query + "'");
+                }
             });
             $(".search-content .search-header-result").text(function() {
                 return $(this).text().replace("Found 1 search results", "Found 1 search result");
