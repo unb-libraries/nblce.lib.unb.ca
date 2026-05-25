910721fd70e73eddeeefe665f0d9d7187f9dd67b
Camilo Villamizar <cvillami@unb.ca>
2026-05-25 15:12:10 -0300

    IN-392 Purce CSS scrollup fix

--- custom/themes/nblce_lib_unb_ca/src/js/scrollup-load-hide.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function () {
-  const button = document.querySelector('.scrollup');
-  if (button && window.scrollY === 0) {
-    button.style.display = 'none';
-  }
-});
